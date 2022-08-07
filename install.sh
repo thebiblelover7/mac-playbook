6eb97ce617074d8ec669b555f7580a7734ab2668
Adriel Sand <sandadriel@outlook.com>
2022-08-07 11:03:23 -0400

    install.sh: do not fail all the script when xcode command line utils are installed

--- install.sh
+++ install.sh
@@ -6,12 +6,18 @@ git_url="https://github.com/thebiblelover7/mac-playbook"
 
 cd /tmp
 
+set +e
 # Install command-line utilities
 xcode-select --install
 
 # Wait for installation to finish
 echo "Press enter when xcode command line utilities are installed:"
 read ready
+
+set -e
+# Upgrade pip and install ansible
+sudo pip3 install --upgrade pip
+pip3 install ansible
 
 # Adding python bin paths to $PATH
 ls -1 $HOME/Library/Python/ | while read python_bins
@@ -21,10 +27,6 @@ done
 
 # Add homebrew bin path to $PATH
 export PATH="/opt/homebrew/bin:$PATH"
-
-# Upgrade pip and install ansible
-sudo pip3 install --upgrade pip
-pip3 install ansible
 
 # Clone git repo with the playbook
 git clone $git_url mac-playbook
